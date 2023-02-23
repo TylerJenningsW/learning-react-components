ae63233d81ed115a84ee9a5012a3a9d9ee5b7aa2
Tyler Jennings <tylerjennings2@outlook.com>
2023-02-23 04:24:13 -0600

    useState hook

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,15 +1,27 @@
+import { useState } from 'react'
 import './ExpenseItem.css'
 import Card from '../UI/Card'
 import ExpenseDate from './ExpenseDate';
 function ExpenseItem(props) {
 
+  // naming convention:
+  // [name to describe current value, mutator / updater]
+  const [title, setTitle] = useState(props.title);
+  // hook always returns an array with two elements
+  // useState(defaultValue);
+
+  const clickHandler = () => {
+    setTitle('Updated.')
+    console.log('test')
+  }
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 }
